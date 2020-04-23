955e4d71d42afa665f261948a248360398881267
owinolawrence <owinolawrence21@gmail.com>
2020-04-23 07:30:13 +0300

    commit credential_test

--- credentials_test.py
+++ credentials_test.py
@@ -1,35 +1,59 @@
 import unittest
 from credentials import Credentials
 
+
 class TestCredentials(unittest.TestCase):
 
-  
- def setUp(self):
-        
-        self.new_credentials = Credentials("facebook","1234") # create credential object
 
+    def setUp(self):
+        self.new_credentials = Credentials("aligedi","instagram","ali","aligedi")
 
- def tearDown(self):
-        
+    def tearDown(self):
         Credentials.credentials_list = []
 
- def test_credentials_instance(self):
 
-        
-        self.assertEqual(self.new_credentials.credentials_name,"facebook")
-        self.assertEqual
-        (self.new_credentials.credentials_password,"1234")
- def test_save_credentials(self):
-        
-        self.new_credentials.save_credentials()
-        self.assertEqual(len(Credentials.credentials_list),)
- def test_delete_credentials(self):
-        
-        self.new_contact.save_contact()
-        test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
-        test_contact.save_contact()
+    def test_init(self):
 
-        self.new_contact.delete_contact()# Deleting a contact object
-        self.assertEqual(len(Contact.contact_list),1)
+
+        self.assertEqual(self.new_credentials.view_passward,"aligedi")
+        self.assertEqual(self.new_credentials.account,"instagram")
+        self.assertEqual(self.new_credentials.login,"ali")
+        self.assertEqual(self.new_credentials.passward,"aligedi")
+
+    def test_save_credential(self):
+
+        self.new_credentials.save_credential()
+        self.assertEqual(len(Credentials.credentials_list),1)
+    def test_multiple_credential(self):
+
+
+        self.new_credentials.save_credential()
+        self.test_credentials = Credentials("osmanhared","instagram","osman","osmanhared")
+        self.test_credentials.save_credential()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+    def test_del_credential(self):
+
+
+        self.new_credentials.save_credential()
+        self.test_credentials = Credentials("osmanhared","instagram","osman","osmanhared")
+        self.test_credentials.save_credential()
+        self.new_credentials.del_credential()
+        self.assertEqual(len(Credentials.credentials_list),1)
+        
+    def test_display_credential(self):
+
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+    
+    def test_exist_credential(self):
+
+
+        self.new_credentials.save_credential()
+        self.test_credentials = Credentials("osmanhared","instagram","osman","osmanhared")
+        self.test_credentials.save_credential()
+
+        credential_exists = Credentials.find_by_account("instagram")
+        self.assertTrue(credential_exists)
+
 if __name__ == '__main__':
     unittest.main()
