e61a92c742db3518c5d32ef3c3d7693ef64d6104
owinolawrence <owinolawrence21@gmail.com>
2020-04-23 08:12:41 +0300

    commit new changes

--- user_test.py
+++ user_test.py
@@ -4,14 +4,14 @@ from user import User
 class Testuser(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User("ali","aligedi")
+        self.new_user = User("Nelvin","123young")
 
 
     def tes_init(sef):
 
 
-        self.assertEqual(self.new_user.username,"ali")
-        self.assertEqual(self.new_user.passward,"aligedi")
+        self.assertEqual(self.new_user.username,"Nelvin")
+        self.assertEqual(self.new_user.passward,"123young")
 
     def test_save_user(self):
 
@@ -23,9 +23,9 @@ class Testuser(unittest.TestCase):
     def test_user_exists(self):
 
         self.new_user.save_user()
-        test_user = User("User","073645")
+        test_user = User("User","0712345")
         test_user.save_user()
-        user_exists = User.user_exists("073645")
+        user_exists = User.user_exists("0712345")
         self.assertTrue(user_exists)
     
 if __name__ == '__main__':
