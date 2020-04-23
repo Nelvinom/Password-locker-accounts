b5f3fc02f5ede35ec7be6f0d4b14e055c59f80a4
owinolawrence <owinolawrence21@gmail.com>
2020-04-23 08:19:02 +0300

    commit credentials

--- user_test.py
+++ user_test.py
@@ -1,33 +1,24 @@
 import unittest
 from user import User
 
-class Testuser(unittest.TestCase):
+
+class TestUser(unittest.TestCase):
+    """Test class that defines test cases for the user class behaviour"""
 
     def setUp(self):
-        self.new_user = User("Nelvin","123young")
+        """Set Up Method to run before each test case to check if the class has been instantiated correctly"""
+        self.new_user = User("NewUser", "12345")
 
-
-    def tes_init(sef):
-
-
-        self.assertEqual(self.new_user.username,"Nelvin")
-        self.assertEqual(self.new_user.passward,"123young")
+    def test_init(self):
+        """Test to ensure that the object is initialized properly"""
+        self.assertEqual(self.new_user.user_name, "NewUser")
+        self.assertEqual(self.new_user.password, "12345")
 
     def test_save_user(self):
-
-
-
+        """Method that tests wether an user credentials have been successfully saved"""
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
+        self.assertEqual(len(User.user_list), 1)
 
-    def test_user_exists(self):
 
-        self.new_user.save_user()
-        test_user = User("User","0712345")
-        test_user.save_user()
-        user_exists = User.user_exists("0712345")
-        self.assertTrue(user_exists)
-    
 if __name__ == '__main__':
     unittest.main()
-
